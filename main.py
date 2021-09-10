bf2de384e7f5c0b6eb58e80ef97eadbe6ccc7014
DarkenVamp <hiteshd1207@gmail.com>
2021-09-10 09:29:54 +0530

    Add main and valo_data modules

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import re
+from modules.valo_image import getImg
+from modules.valo_data import getData
+from telegram import ChatAction
+from telegram.ext import Updater, CommandHandler
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+TOKEN = os.environ.get('TOKEN')
+updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
+
+def start(update, _):
+    update.message.reply_text('Use "/valo <username#tagline>" to get stats.')
+
+def valo(update, context):
+    context.bot.sendChatAction(update.message.chat_id, ChatAction.TYPING)
+    name = re.sub(r'\/\w+\s', '', update.effective_message.text)
+    data = getData(name)
+    if isinstance(data, dict):
+        update.message.reply_photo(getImg(data))
+    else:
+        update.message.reply_text(data)
+
+dispatcher.add_handler(CommandHandler('start', start, run_async=True))
+dispatcher.add_handler(CommandHandler('valo', valo, run_async=True))
+
+updater.start_polling()
+updater.idle()
---
+++ modules/valo_data.py
@@ -0,0 +1,51 @@
+import requests
+profile = 'https://api.tracker.gg/api/v2/valorant/standard/profile/riot/'
+matches = profile.replace('profile', 'matches')
+header = {'User-Agent': 'Mozilla/5.0'}
+
+def getShots(name):
+    heads = body = legs = 0
+    r = requests.get(matches + name.replace('#', '%23'), headers=header).json()
+    if 'errors' in r:
+        return r['errors'][0]['message']
+
+    for i in range(20):
+        stats = r['data']['matches'][i]['segments'][0]['stats']
+        heads += stats['dealtHeadshots']['value']
+        body += stats['dealtBodyshots']['value']
+        legs += stats['dealtLegshots']['value']
+
+    total = heads + body + legs
+    return (round(heads*100/total, 2), round(body*100/total, 2), round(legs*100/total, 2))
+
+def getData(name):
+    r = requests.get(profile + name.replace('#', '%23'), headers=header).json()
+    if 'errors' in r:
+        return r['errors'][0]['message']
+
+    agents = []
+    d = {'name': name}
+    for x in r['data']['segments']:
+        if x['type'] == 'agent':
+            agents.append({'name': x['metadata']['name'], 'role':x['metadata']['role'], 'matchesPlayed': x['stats']['matchesPlayed']['value'],
+                        'matchesWinPct': x['stats']['matchesWinPct']['displayValue'], 'kDRatio': x['stats']['kDRatio']['displayValue']})
+
+    agents.sort(key=lambda x: x['matchesPlayed'], reverse=True)
+    d['agents'] = agents[:3]
+
+    for x in r['data']['segments']:
+        if x['metadata']['name'] == 'Competitive':
+            stats = x['stats']
+            d['timePlayed'] = stats['timePlayed']['displayValue']
+            d['kDRatio'] = stats['kDRatio']['displayValue']
+            d['rank'] = stats['rank']['metadata']['tierName']
+            d['kills'] = stats['kills']['displayValue']
+            d['deaths'] = stats['deaths']['displayValue']
+            d['matchesPlayed'] = stats['matchesPlayed']['displayValue']
+            d['matchesWon'] = stats['matchesWon']['displayValue']
+            d['matchesLost'] = stats['matchesLost']['displayValue']
+            d['headshots'] = stats['headshots']['displayValue']
+            d['headPct'], d['bodyPct'], d['legPct'] = getShots(name)
+            return d
+
+    return "You haven't played Competitive in recent 20 matches."
