4174b96f4116e5052fd36e6b625189ec7c288349
Hitesh Deswal <hiteshd1207@gmail.com>
2022-05-27 13:15:53 +0530

    valo_data: use cookie for fetching data

--- modules/valo_data.py
+++ modules/valo_data.py
@@ -1,11 +1,17 @@
 import requests
+import os
 profile = 'https://api.tracker.gg/api/v2/valorant/standard/profile/riot/'
 matches = profile.replace('profile', 'matches')
-header = {'User-Agent': 'Mozilla/5.0', 'Referer': 'https://tracker.gg'}
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0'
+}
+cookie = {'__cf_bm': os.environ.get('API_COOKIE')}
+
 
 def getShots(name):
     heads = body = legs = 0
-    r = requests.get(matches + name.replace('#', '%23'), headers=header).json()
+    r = requests.get(matches + name.replace('#', '%23'),
+                     headers=header, cookies=cookie).json()
     if 'errors' in r:
         return r['errors'][0]['message']
 
@@ -18,8 +24,10 @@ def getShots(name):
     total = heads + body + legs
     return (round(heads*100/total, 2), round(body*100/total, 2), round(legs*100/total, 2))
 
+
 def getData(name):
-    r = requests.get(profile + name.replace('#', '%23'), headers=header).json()
+    r = requests.get(profile + name.replace('#', '%23'),
+                     headers=header, cookies=cookie).json()
     if 'errors' in r:
         return r['errors'][0]['message']
 
@@ -27,8 +35,8 @@ def getData(name):
     d = {'name': name}
     for x in r['data']['segments']:
         if x['type'] == 'agent':
-            agents.append({'name': x['metadata']['name'], 'role':x['metadata']['role'], 'matchesPlayed': x['stats']['matchesPlayed']['value'],
-                        'matchesWinPct': x['stats']['matchesWinPct']['displayValue'], 'kDRatio': x['stats']['kDRatio']['displayValue']})
+            agents.append({'name': x['metadata']['name'], 'role': x['metadata']['role'], 'matchesPlayed': x['stats']['matchesPlayed']['value'],
+                           'matchesWinPct': x['stats']['matchesWinPct']['displayValue'], 'kDRatio': x['stats']['kDRatio']['displayValue']})
 
     agents.sort(key=lambda x: x['matchesPlayed'], reverse=True)
     d['agents'] = agents[:3]
